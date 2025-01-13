7ef6ef51e67dcd3c231cd50ef475b02180ce5d0d
Anubha Rajput <88902737+anubha193@users.noreply.github.com>
2025-01-13 13:48:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,10 +60,9 @@ async function getMongoUrl() {
 }
 
 (async () => {
-  const mongoUrl = 'mongodb+srv://User123:<Anubha88@@#>@cluster0.fu8ny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  const mongoUrl = 'mongodb+srv://User123:Anubha88%40%40%23@cluster0.fu8ny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; //'mongodb://127.0.0.1:27017';
   const dbName = 'fingerprintDB';
   const collectionName = 'records';
-  console.log(mongoUrl);
 
   const client = new MongoClient(mongoUrl);
   let db, collection;
